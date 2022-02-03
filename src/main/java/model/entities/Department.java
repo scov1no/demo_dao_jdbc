632d94a7baa696132d3e97254f77f0feb6a2079d
DevEddi <scovinowork@gmail.com>
2022-02-03 10:07:16 -0300

    department entity class
    entity class checklist: Attributes, COnstructors, Getters/Setters, hashCOde and equals, toString, implements Serializable

---
+++ src/main/java/model/entities/Department.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Department(){
+
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department)) return false;
+
+        Department that = (Department) o;
+
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        return getName() != null ? getName().equals(that.getName()) : that.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
